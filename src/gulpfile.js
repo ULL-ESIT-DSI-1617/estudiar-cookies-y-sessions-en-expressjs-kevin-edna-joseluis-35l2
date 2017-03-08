295980eb6ede82691023223a8e66cb4d3056879e
Edna Galiano <ednagc.11@gmail.com>
2017-03-08 23:41:37 +0000

    Añadiendo tarea1 del /src/gulpfile.js

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,8 +2,21 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 
+
+// Tarea ejemplo1 -------------------------------------------------------
+
+gulp.task('ejemplo2', function(){
+	return gulp.src('.').pipe(shell(['node ejemplo_cap8.js']));
+});
+
+
+
 // Tarea ejemplo2 -------------------------------------------------------
 
 gulp.task('ejemplo2', function(){
 	return gulp.src('.').pipe(shell(['node ejemplo_cap9.js']));
 });
+
+
+// Tarea Default -------------------------------------------------------
+gulp.task('default', ['ejemplo1', 'ejemplo2']);
