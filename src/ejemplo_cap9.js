091db975948855850cb84abdc380b27348f3f85a
opelu <opelu8@gmail.com>
2017-03-08 23:23:10 +0000

    Actualizando ejemplo_cap9.js (Autenticación y autorización mediante sesiones en Express)

---
+++ src/ejemplo_cap9.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+    app = express(),
+    session = require('express-session');
+app.use(session({
+    secret: '2C44-4D44-WppQ38S',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Authentication and Authorization Middleware
+var auth = function(req, res, next) {
+    if (req.session && req.session.user === "pepe" && req.session.admin)
+        return next();
+    else
+        return res.sendStatus(401);
+};
+
+// Login endpoint
+app.get('/login', function (req, res) {                         // https://dsi-opelu.c9users.io/login?username=pepe&password=pepespassword
+    if (!req.query.username || !req.query.password) {
+        res.send('Login Incorrecto');    
+    } else if(req.query.username === "pepe" || req.query.password === "pepespassword") {
+        req.session.user = "pepe";
+        req.session.admin = true;
+        res.send("Login correcto!");
+    }
+});
+
+// Logout endpoint
+app.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.send("Logout exitoso!");
+});
+
+// Get content endpoint
+app.get('/content', auth, function (req, res) {
+    res.send("Si estás viendo esto es que has hecho login correctamente");
+});
+
+app.listen(8080);
+console.log("Servidor corriendo en: http://localhost:8080");
