f88c72562ff258d65cb78a8a47ad6c9490455841
alu0100821390 <alu0100821390@ull.edu.es>
2017-03-13 20:57:20 +0000

    Despliegue gh-pages

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+// instanciar
+var app = express();
+app.set('port', (process.env.PORT || 8082));
+
+
+// Ruteo
+app.use(express.static('.'));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+
+
+// Escuchar
+var server = app.listen(app.get('port'), function () {
+    console.log('Servidor escuchando en el puerto %s', app.get('port'));
+});
